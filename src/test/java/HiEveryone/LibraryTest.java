3068d072edb8e77492ee80832418c34911df3def
Piyush Yadav <31996687+piyush5989@users.noreply.github.com>
2026-02-23 23:04:58 +0530

    Refactor LibraryTest to use Assert class explicitly

--- src/test/java/HiEveryone/LibraryTest.java
+++ src/test/java/HiEveryone/LibraryTest.java
@@ -4,11 +4,12 @@
 package HiEveryone;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
+import static org.junit.Assert;
 
 public class LibraryTest {
-    @Test public void testSomeLibraryMethod() {
+    @Test
+    public void testSomeLibraryMethod() {
         Library classUnderTest = new Library();
-        assertTrue("someLibraryMethod should return 'true'", classUnderTest.someLibraryMethod());
+        Assert.assertTrue("someLibraryMethod should return 'true'", classUnderTest.someLibraryMethod());
     }
 }
